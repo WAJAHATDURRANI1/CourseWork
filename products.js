d0ed37ec7dd6bfd8938baab8099d05326dffe0cf
WajahatDurrani828 <wajahat8286342@gmail.com>
2024-11-27 17:07:27 +0000

    first push

---
+++ products.js
@@ -0,0 +1,12 @@
+let lessons = [
+                    { id: 1, subject: "Math", location: "Room 101", price: 20, spaces: 5, image: "https://via.placeholder.com/150" },
+                    { id: 2, subject: "English", location: "Room 102", price: 25, spaces: 3, image: "https://via.placeholder.com/150" },
+                    { id: 3, subject: "Science", location: "Room 103", price: 30, spaces: 0, image: "https://via.placeholder.com/150" },
+                    { id: 4, subject: "History", location: "Room 104", price: 15, spaces: 4, image: "https://via.placeholder.com/150" },
+                    { id: 5, subject: "Art", location: "Room 105", price: 40, spaces: 2, image: "https://via.placeholder.com/150" },
+                    { id: 6, subject: "Music", location: "Room 106", price: 35, spaces: 5, image: "https://via.placeholder.com/150" },
+                    { id: 7, subject: "PE", location: "Room 107", price: 10, spaces: 1, image: "https://via.placeholder.com/150" },
+                    { id: 8, subject: "Biology", location: "Room 108", price: 45, spaces: 5, image: "https://via.placeholder.com/150" },
+                    { id: 9, subject: "Chemistry", location: "Room 109", price: 50, spaces: 4, image: "https://via.placeholder.com/150" },
+                    { id: 10, subject: "Physics", location: "Room 110", price: 60, spaces: 0, image: "https://via.placeholder.com/150" },
+                ]
